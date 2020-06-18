bdb12363e32dcfe3aa891dbd0b96bd8b0631ca04
Rachel Fritz <rachelfritz0@gmail.com>
2020-06-18 06:07:47 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -21,14 +21,15 @@ var fight = function(enemyName) {
       if (confirmSkip) {
         window.alert(playerName + ' has decided to skip this fight. Goodbye!');
         // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
         console.log("playerMoney", playerMoney)
         break;
       }
     }
 
     // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+    enemyHealth = Math.max(0, enemyHealth - damage);
     console.log(
       playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
     );
@@ -47,7 +48,8 @@ var fight = function(enemyName) {
     }
 
     // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+    playerHealth = Math.max(0, playerHealth - damage);
     console.log(
       enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
     );
@@ -74,9 +76,16 @@ var startGame = function() {
 
       var pickedEnemyName = enemyNames[i];
 
-      enemyHealth = 50;
+      // ask about this segment section 3.4.4
+      enemyHealth = Math.floor(Math.random() * 21) + 40;
 
       fight(pickedEnemyName);
+
+    var randomNumber = function (min, max) {
+      var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+    return value;
+  };
 
       if (playerHealth > 0 && i < enemyNames.length - 1) { 
         var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
