dea71389602c5ef45b7d68f241d67849229b848e
Rachel Fritz <rachelfritz0@gmail.com>
2020-06-19 08:10:30 -0500

    fix bugs

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,15 @@
+var getPlayerName = function () {
+  var name = "";
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+  console.log("Your robot's name is" + name);
+  return name;
+}
+
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  // name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
@@ -114,7 +124,6 @@ var startGame = function() {
   for (var i = 0; i < enemyNames.length; i++) {
     if (playerInfo.health > 0) {
       window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
-
       var pickedEnemyName = enemyNames[i];
 
       enemyHealth = 50;
